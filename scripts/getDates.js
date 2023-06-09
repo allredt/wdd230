1136b1a295b12cfaa447f57cb01fa86714a675af
allredt <allred.trudy@gmail.com>
2023-06-09 11:20:24 -0600

    update with copy write year code

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,6 +2,5 @@ let text = document.lastModified;
 
 document.getElementById("lastModified").innerHTML = `Last Modified: ${text}`
 
-let year = document.lastModified
-year = year %Y
-document.getElementById("year").innerHTML = `Last Modified: ${year}`
+let year = new Date(document.lastModified)
+document.getElementById("year").innerHTML = `&copy; ${year.getFullYear()}`
