1d8ddf9460b57679313e6b9414723bfb2d2eda79
allredt <allred.trudy@gmail.com>
2023-06-17 17:27:07 -0600

    updated with X symbol

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,6 @@
+let text = document.lastModified;
+
+document.getElementById("lastModified").innerHTML = `Last Modified: ${text}`
+
+let year = new Date(document.lastModified)
+document.getElementById("year").innerHTML = `&copy; ${year.getFullYear()}`
