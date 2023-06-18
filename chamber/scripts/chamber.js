d3b1b988261a88e997c54d46f36f10419f210f36
allredt <allred.trudy@gmail.com>
2023-06-17 21:21:10 -0600

    chamber homepage

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -4,3 +4,26 @@ document.getElementById("lastModified").innerHTML = `Last Modified: ${text}`
 
 let year = new Date(document.lastModified)
 document.getElementById("year").innerHTML = `&copy; ${year.getFullYear()}`
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
