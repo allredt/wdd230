c01b32124e1658b449d89fc9932fef231f087b1a
allredt <allred.trudy@gmail.com>
2023-06-26 15:04:34 -0600

    Added Visit counter

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,9 +1,32 @@
+let newVisit = Date.now();
+
+let lastVisit = Date.now();
+
+let elapsed = (lastVisit-newVisit)/1000;
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits == 0) {
+	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+}if (elapsed <= 86400) {
+	visitsDisplay.textContent = `Back so soon! Awesome!`;
+}
+ else {
+	visitsDisplay.textContent = `Visits: ${numVisits()}`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
 let text = document.lastModified;
 
-document.getElementById("lastModified").innerHTML = `Last Modified: ${text}`
+document.getElementById("lastModified").innerHTML = `Last Modified: ${text}`;
 
 let year = new Date(document.lastModified)
-document.getElementById("year").innerHTML = `&copy; ${year.getFullYear()}`
+document.getElementById("year").innerHTML = `&copy; ${year.getFullYear()}`;
 
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
@@ -27,24 +50,3 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "☑️";
 	}
 });
-
-
-// Initialize display element 
-// variable
-const visitsDisplay = document.querySelector(".visits");
-
-// Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-
-// Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
-}
-
-// increment the number of visits by one.
-numVisits++;
-
-// store the new visit total into localStorage, key=numVisits-ls
-localStorage.setItem("numVisits-ls", numVisits);
