fcaefa41c6646dc48c033bc85a3f25ced3eb2dca
allredt <allred.trudy@gmail.com>
2023-07-01 15:39:29 -0600

    removed getdate.js and combined with index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
+
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
@@ -20,6 +21,35 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "☑️";
 	}
 });
+
+//-----------password check-----------//
+// const kp1 = document.querySelector("#keyphrase");
+// const kp2 = document.querySelector("#keyphrase2");
+// const message = document.querySelector("#formmessage");
+ 
+// kp2.addEventListener("focusout", checkSame);
+
+// // This should be refactored.
+// function checkSame() {
+// 	if (kp1.value !== kp2.value) {
+// 		message.textContent = "❗Key Phrases DO NOT MATCH!";
+// 		message.style.visibility = "show";
+// 		kp2.style.backgroundColor = "#fff0f3";
+// 		kp2.value = "";
+// 		kp2.focus();
+// 	} else {
+// 		message.style.display = "none";
+// 		kp2.style.backgroundColor = "#fff";
+// 		kp2.style.color = "#000";
+// 	}
+// }
+
+let text = document.lastModified;
+
+document.getElementById("lastModified").innerHTML = `Last Modified: ${text}`
+
+let year = new Date(document.lastModified)
+document.getElementById("year").innerHTML = `&copy; ${year.getFullYear()}`
 
 // Initialize display element 
 // variable
@@ -41,24 +71,4 @@ numVisits++;
 // store the new visit total into localStorage, key=numVisits-ls
 localStorage.setItem("numVisits-ls", numVisits);
 
-//-----------password check-----------//
-const kp1 = document.querySelector("#keyphrase");
-const kp2 = document.querySelector("#keyphrase2");
-const message = document.querySelector("#formmessage");
 
-kp2.addEventListener("focusout", checkSame);
-
-// This should be refactored.
-function checkSame() {
-	if (kp1.value !== kp2.value) {
-		message.textContent = "❗Key Phrases DO NOT MATCH!";
-		message.style.visibility = "show";
-		kp2.style.backgroundColor = "#fff0f3";
-		kp2.value = "";
-		kp2.focus();
-	} else {
-		message.style.display = "none";
-		kp2.style.backgroundColor = "#fff";
-		kp2.style.color = "#000";
-	}
-}
