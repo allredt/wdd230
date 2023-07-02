1485282d21967daa34bb64a78912518560576ad6
allredt <allred.trudy@gmail.com>
2023-07-01 18:46:49 -0600

    adjusting language on discover page

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,27 +1,3 @@
-let newVisit = Date.now();
-
-let lastVisit = Date.now();
-
-let elapsed = (lastVisit-newVisit)/1000;
-
-const visitsDisplay = document.querySelector(".visits");
-
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-
-if (numVisits == null || numVisits == 0) {
-	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
-} if (elapsed <= 86400) {
-	visitsDisplay.textContent = `Back so soon! Awesome!`;
-} if (elapsed >= 86400 && elapsed <= 172800) {
-	visitsDisplay.textContent = `You last visited ${numVisits()} day ago.`;
-} else {
-	visitsDisplay.textContent = `You last visited ${numVisits()} days ago.`;
-}
-
-numVisits++;
-
-localStorage.setItem("numVisits-ls", numVisits);
-
 let text = document.lastModified;
 
 document.getElementById("lastModified").innerHTML = `Last Modified: ${text}`;
