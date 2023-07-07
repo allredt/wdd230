ba2660921a2522bd53529e7883c321caa2cee968
allredt <allred.trudy@gmail.com>
2023-07-07 14:51:21 -0600

    adding directory

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -2,74 +2,48 @@
 const url = 'https://allredt.github.io/wdd230/chamber/data/members.json';
 const cards = document.querySelector('#cards');
 
-const displaymembers = (members) => {
+async function getMemberData () {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.members);
+    displayMembers(data.members);
+}
+
+const displayMembers = (members) => {
     members.forEach((member) => {
 
         let card = document.createElement('section');
         let fullName = document.createElement('h2');
         let portrait = document.createElement('img');
+        let address = document.createElement('p')
+        let website = document.createElement('a')
+        let membership = document.createElement('p');
 
         fullName.textContent = `${member.name}`;
+        console.log(fullName)
+
+        address.textContent = `${member.address}`;
+        console.log(website);
+
+        membership.textContent = `${member.membershipLevel}`
+
 
         portrait.setAttribute('src', member.image);
-        portrait.setAttribute('alt', `Portrait of ${member.name}`);
+        portrait.setAttribute('alt', `${member.name}`);
         portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+        portrait.setAttribute('width', '500');
+        portrait.setAttribute('height', '250');
+
+        website.setAttribute('href', member.website);
+        website.textContent = `${member.website}`;
 
         card.appendChild(fullName);
         card.appendChild(portrait);
+        card.appendChild(address);
+        card.appendChild(website);
+        card.appendChild(membership);
         cards.appendChild(card);
     });
 }
-
-async function getMemberData () {
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.table(data.members);
-    displayMembers(data.members);
-}
-
-// const displaymembers = (members) => {
-//     members.forEach((member) => {
-
-//         let card = document.createElement('section');
-//         let fullName = document.createElement('h2');
-//         let portrait = document.createElement('img');
-
-//         fullName.textContent = `${member.name}`;
-
-//         portrait.setAttribute('src', member.image);
-//         portrait.setAttribute('alt', `Portrait of ${member.name}`);
-//         portrait.setAttribute('loading', 'lazy');
-//         portrait.setAttribute('width', '340');
-//         portrait.setAttribute('height', '440');
-
-//         card.appendChild(fullName);
-//         card.appendChild(portrait);
-//         cards.appendChild(card);
-//     });
-// }
-
-// const displayProphets = (prophets) => {
-//     prophets.forEach((prophet) => {
-
-        // let card = document.createElement('section');
-        // let fullName = document.createElement('h2');
-        // let portrait = document.createElement('img');
-
-        // fullName.textContent = `${prophet.name} ${prophet.lastname}`;
-
-        // portrait.setAttribute('src', prophet.imageurl);
-        // portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
-        // portrait.setAttribute('loading', 'lazy');
-        // portrait.setAttribute('width', '340');
-        // portrait.setAttribute('height', '440');
-
-        // card.appendChild(fullName);
-        // card.appendChild(portrait);
-        // cards.appendChild(card);
-//     });
-// }
 
 getMemberData();
