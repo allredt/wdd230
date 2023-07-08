3e1049e36216613815528090ff44963d11b4049f
allredt <allred.trudy@gmail.com>
2023-07-08 16:37:04 -0600

    updating chamber weather

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,35 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=39.73&lon=-104.83&units=imperial&appid=e8f65f39b710ea837be37a47db5be40d'
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    }catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt',`${data.weather[0].description}`);
+    captionDesc.textContent = `${desc}`;
+}
+
+
 let text = document.lastModified;
 
 document.getElementById("lastModified").innerHTML = `Last Modified: ${text}`;
